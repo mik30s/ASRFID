87b0cac73137b98fa888cd59b696cb0662c8b082
Michael Osei <mosei27@live.com>
2017-08-23 16:36:33 -0500

    Add database connection settings

--- webservice/index.js
+++ webservice/index.js
@@ -1,8 +1,26 @@
 var express = require('express')
-var app = express();
+var mysql = require('mysql');
 var ejs = require('ejs');
+var app = express();
+var auth = require(__dirname + '/auth.json');
+var dbConnection = null;
 
 app.use(express.static(__dirname + 'public/'));
+// initalize data base connection
+app.use(function(req, resp, next){
+	dbConnection = mysql.createConnection({
+		host: "localhost",
+		user: auth.dbUser,
+		password: auth.dbPass,
+	});
+
+	dbConnection.connect(function(err){
+		if (err) throw err;
+		console.log("connected!");
+	});
+
+	next();
+});
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -13,7 +31,12 @@ app.get('/', function(req, resp, next) {
 	resp.render('index', {title: 'ASRFID stats'});
 });
 
+// get all patient data for client side processing
+app.get('/patients_data', function(req, resp, next){
 
+});
+
+// start server
 app.listen(process.env.PORT || 3000, function(){
 	console.log('Listening on ' + (process.env.PORT || 3000));
 });
